5be2ab06b017ed10fa8f3bd2775c61f6bfc87eab
Boskicl <ljuboslavboskic@gmail.com>
2020-09-23 19:01:35 -0700

    gets to captcha

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 keys = {
-    "productURL" : "https://www.nvidia.com/en-us/shop/geforce/?page=1&limit=9&locale=en-us",
+    "productURL" : "https://www.newegg.com/msi-geforce-rtx-2080-super-rtx-2080-super-gaming-x-trio/p/N82E16814137442?Description=2080&cm_re=2080-_-14-137-442-_-Product&quicklink=true",
+    # https://www.newegg.com/msi-geforce-rtx-3080-rtx-3080-gaming-x-trio-10g/p/N82E16814137597?Description=3080&cm_re=3080-_-14-137-597-_-Product&quicklink=true
     "firstname" : "Ljuboslav",
     "lastname" : "Boskic",
     "streetad" : "add",
--- src/setup.py
+++ src/setup.py
@@ -20,10 +20,41 @@ class Buy:
         driver = self.driver
         driver.get(self.url)
         sleep(1)
+
     def addcart(self):
-        gosite(self)
+        driver = self.driver
+        gosite = self.gosite()
+        sleep(2)
+        try:
+            driver.find_element_by_id("landingpage-cart")
+            print("Found Add to Cart.")
+            try:
+                driver.find_element_by_css_selector("#landingpage-cart > div > div.nav-col.call-to-action.call-to-action-main-product").click()
+                print("Clicked Add to Cart.")
+                sleep(0.2)
+                if driver.find_elements_by_class_name("centerPopup-body") is not None:
+                    driver.find_element_by_css_selector("#custom > div > div.centerPopup-bottom > div > div > div > button.btn.button-decline.btn-cancel").click()
+                    sleep(5)
+                    try:
+                        driver.find_element_by_css_selector("#captcha > div > div.geetest_btn > div.geetest_radar_btn > div.geetest_radar_tip").click()
+                        print("clicked: Human")
+                    except:
+                        print("HERE")
+                        driver.find_element_by_id("recaptcha-anchor").click()
+                        print("clicked: Verify")
+                else:
+                    print("error 1")
+            except:
+                print("error 2")
+        except:
+            print("error 3")
+        # Destroy popup
+        #driver.find_elements_by_class_name("close").click()
+
+
+
 
 
 if __name__ == '__main__':
     buy = Buy(keys)
-    buy.site()
+    buy.addcart()
